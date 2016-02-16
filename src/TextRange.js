b90654f0abe89473d97c0c2a6c08c9974d9ef340
daneryl <daneryl@gmail.com>
2016-02-15 20:12:08 -0500

    TextRange.restore returns a Range object based on start/end positions and a rootNode

--- src/TextRange.js
+++ src/TextRange.js
@@ -1,18 +1,55 @@
 export default {
 
-  getSelected: (containerEl) => {
+  getSelected: (rootNode) => {
 
-    var range = window.getSelection().getRangeAt(0);
-    var preSelectionRange = range.cloneRange();
-    preSelectionRange.selectNodeContents(containerEl);
-    preSelectionRange.setEnd(range.startContainer, range.startOffset);
-    var start = preSelectionRange.toString().length;
+    let selectedRange = window.getSelection().getRangeAt(0);
+    let start = getStartCharacterFor(selectedRange, rootNode);
 
     return {
       start: start,
-      end: start + range.toString().length
+      end: start + selectedRange.toString().length
     };
 
+  },
+
+  restore: (textPosition, rootNode) => {
+    let charIndex = 0, range = document.createRange();
+    let nodeStack = [rootNode], node;
+
+    while (node = nodeStack.pop()) {
+      if (node.nodeType == 3) {
+
+        var nextCharIndex = charIndex + node.length;
+
+        if (textPosition.start >= charIndex && textPosition.start <= nextCharIndex) {
+          range.setStart(node, textPosition.start - charIndex);
+        }
+
+        if (textPosition.end >= charIndex && textPosition.end <= nextCharIndex) {
+          range.setEnd(node, textPosition.end - charIndex);
+          break;
+        }
+
+        charIndex = nextCharIndex;
+
+      } else {
+
+        var i = node.childNodes.length;
+        while (i--) {
+          nodeStack.push(node.childNodes[i]);
+        }
+
+      }
+    }
+
+    return range;
   }
 
+}
+
+function getStartCharacterFor(range, rootNode) {
+  let preRange = range.cloneRange();
+  preRange.selectNodeContents(rootNode);
+  preRange.setEnd(range.startContainer, range.startOffset);
+  return preRange.toString().length;
 }
--- src/specs/TextRange.spec.js
+++ src/specs/TextRange.spec.js
@@ -3,39 +3,45 @@ import $ from 'jquery';
 
 describe('TextRange', () => {
 
+  let rootNode, range;
+
+  beforeEach(() => {
+    $(document.body).append(
+      '<div id="root">outside the container'+
+      '<div id="container">'+
+      '1234<span>67891</span>'+
+      '</div>'+
+      '</div>'
+    );
+
+    rootNode = $('#container')[0];
+
+    range = document.createRange();
+    range.setStart(rootNode.childNodes[0], 2);
+    range.setEnd(rootNode.childNodes[1].childNodes[0], 1);
+
+    let sel = window.getSelection();
+    sel.removeAllRanges();
+    sel.addRange(range);
+
+  });
+
+  afterEach(() => {
+    $(rootNode).remove();
+  });
+
   describe('getSelected()', () => {
-
-    let rootNode, range;
-
-    beforeEach(() => {
-      $(document.body).append(
-        '<div id="root">'+
-        '<div id="container">'+
-        '1234<span>67891</span>'+
-        '</div>'+
-        '</div>'
-      );
-
-      rootNode = $('#container')[0];
-
-      range = document.createRange();
-      range.setStart(rootNode.childNodes[0], 2);
-      range.setEnd(rootNode.childNodes[1].childNodes[0], 1);
-
-      let sel = window.getSelection();
-      sel.removeAllRanges();
-      sel.addRange(range);
-
-    });
-
-    afterEach(() => {
-      $(rootNode).remove();
-    });
-
     it('should return an object with end/start number of characters based on a root node', () => {
       let range = TextRange.getSelected(rootNode);
       expect(range).toEqual({start:2, end: 5});
     });
-
   });
+
+  describe('restore()', () => {
+    it('should create a Range object based on a start/end character position object', () => {
+      let restoredRange = TextRange.restore({start:2, end:5}, rootNode);
+      expect(restoredRange.toString()).toBe(range.toString());
+    });
+  });
+
 });
